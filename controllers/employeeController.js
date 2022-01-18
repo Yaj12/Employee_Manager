29c7b754abdd4d229ea2a52e5f1c8b958a391f03
9p6b <9p6b@fresnou.org>
2022-01-18 08:30:55 -0800

    Your Turn

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -20,7 +20,8 @@ module.exports.renderAddEmployeeForm = function(req, res){
                 state:states[0],
                 zip:'',
                 phonenumber:'',
-                yearhired:''
+                yearhired:'',
+                age:''
             },
             jobtitles,
             stateslist:states
@@ -39,7 +40,8 @@ module.exports.addEmployee = async function(req, res){
             state: req.body.state,
             zip: req.body.zip,
             phonenumber: req.body.phonenumber,
-            yearhired: req.body.yearhired
+            yearhired: req.body.yearhired,
+            age: req.body.age
         }
     );
     res.redirect('/');
@@ -66,7 +68,8 @@ module.exports.updateEmployee = async function(req, res){
             state: req.body.state,
             zip: req.body.zip,
             phonenumber: req.body.phonenumber,
-            yearhired: req.body.yearhired
+            yearhired: req.body.yearhired,
+            age: req.body.age
         },
                 {
             where: {
